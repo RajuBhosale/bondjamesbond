bd247ab4cf6d4064a2ed441732020c1e2543626d
Raju Bhosale <raju@blimp.co.in>
2012-07-28 14:55:13 +0530

    basis Layout functionality

---
+++ js/_JBondSite.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(function () {
+    var filter;
+    $('#filters > li').on('click', function (event) {
+        filter = $(this).attr('id');
+        //calculate max
+        var max = 0;
+        $.each(bonds, function (index, thisbond) {
+            $('#' + thisbond.name + ' .stats').html(thisbond[filter]);
+            if (thisbond[filter] > max) {
+                max = thisbond[filter];
+            }
+        });
+        
+        //calculate width of container in which this has to be display
+        var widthOfStats = $('#bondlayout .stats').first().width();
+        console.log( widthOfStats +"  "+max);
+
+
+        $.each(bonds, function (index, thisbond) {
+            switch(filter){
+                case 'films':
+                    var widthShouldBe = widthOfStats/max ;
+                    console.log(thisbond.name+" films :"+ thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - films");
+
+                    break;
+                case 'kills' : 
+
+                    var widthShouldBe = widthOfStats / max;
+                    console.log(thisbond.name + " kills :" + thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - kills");
+
+                    break;
+                case 'kissed' : 
+
+                    var widthShouldBe = widthOfStats / max;
+                    console.log(thisbond.name + " kissed :" + thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - kissed");
+                    break;
+                
+                case 'iSlept' : 
+
+                    var widthShouldBe = widthOfStats / max;
+                    console.log(thisbond.name + " iSlept :" + thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - iSlept");
+                    break;
+                case 'eSlept' : 
+
+                    var widthShouldBe = widthOfStats / max;
+                    console.log(thisbond.name + " eSlept :" + thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - eSlept");
+                    break;
+                case 'believed_dead' : 
+
+                    var widthShouldBe = widthOfStats / max;
+                    console.log(thisbond.name + " believed_dead :" + thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - belived_dead");
+                    break;
+                case 'bond_james_bond' : 
+
+                    var widthShouldBe = widthOfStats/max ;
+                    console.log(thisbond.name + " bond_james_bond :" + thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - bond_james_bond");
+                    break;
+                case 'martinis_drunk' : 
+
+                    var widthShouldBe = widthOfStats / max;
+                    console.log(thisbond.name + " martinis_drunk :" + thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - martinis_drunk");
+                    break;
+                case 'rotten_tomato':
+
+                    var widthShouldBe = widthOfStats / max;
+                    console.log(thisbond.name + " rotten_tomato :" + thisbond[filter] + " average_Count: " + thisbond[filter] / max + " widthShouldBe :" + widthShouldBe + " - rotten_tomato");
+                    break;
+
+            
+            }
+
+        });
+
+
+    });
+
+});
---
+++ js/_references.js
@@ -0,0 +1,3 @@
+﻿/// <reference path="jquery-1.7.2.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="bonds.js" />
